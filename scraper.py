83f154fae03388a6d74b38b3be359ee0b9a4b676
crunchyLuke <luke.charbonneau@crunchtec.com>
2020-01-06 12:50:25 -0700

    init

---
+++ scraper.py
@@ -0,0 +1,92 @@
+import requests
+import bs4
+import json
+
+
+def get_snow_report(search_url_loc):
+    def extract_digits(digit_str):
+        return ''.join(x for x in digit_str if x.isdigit() or x == '.' or x == '-')
+
+    user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 " \
+                 "Safari/537.36 "
+    # US english
+    language = "en-US,en;q=0.5"
+
+    session = requests.Session()
+    session.headers['User-Agent'] = user_agent
+    session.headers['Accept-Language'] = language
+    session.headers['Content-Language'] = language
+    search_url = 'snow+report+' + search_url_loc
+    html = session.get(url="https://www.google.com/search?q=" + search_url)
+
+    soup = bs4.BeautifulSoup(html.text, "html.parser")
+
+    result: dict = {
+        "snowfall72": None,
+        "snowfall48": None,
+        "snowfall24": None,
+        "snowDescription": None,
+        "runsOpen": None,
+        "liftsOpen": None,
+        "base": None,
+        "baseTemperature": None,
+        "summit": None,
+        "summitTemperature": None,
+        "todaySnowForecast": None,
+        "todayHighTemperature": None,
+    }
+
+    try:
+        has_weather = bool(soup.find_all("td", attrs={"class": "cuG6ob"}))
+
+        if has_weather:
+            all_snowfall = soup.find_all("span", attrs={"class": "pjam0"})
+            all_toplevel = soup.find_all("div", attrs={"class": "cuG6ob"})
+            all_midlevel = soup.find_all("td", attrs={"class": "cuG6ob"})
+            all_forecast_snow = soup.find_all("span", attrs={"class": "TSjJbc"})
+            high_temperatures = soup.find_all("span", attrs={"class": "ZSvVgb"})
+
+            if all_snowfall:
+                result["snowfall72"] = extract_digits(all_snowfall[0].text)
+                result["snowfall48"] = extract_digits(all_snowfall[1].text)
+                result["snowfall24"] = extract_digits(all_snowfall[2].text)
+            if all_toplevel:
+                result["snowDescription"] = all_toplevel[0].text
+                result["runsOpen"] = all_toplevel[1].text
+                result["liftsOpen"] = all_toplevel[2].text
+            if all_midlevel:
+                result["base"] = extract_digits(all_midlevel[0].text)
+                result["baseTemperature"] = extract_digits(all_midlevel[1].text)
+                result["summit"] = extract_digits(all_midlevel[2].text)
+                result["summitTemperature"] = extract_digits(all_midlevel[3].text)
+            if all_forecast_snow:
+                result["todaySnowForecast"] = extract_digits(all_forecast_snow[0].text)
+            if high_temperatures:
+                result["todayHighTemperature"] = extract_digits(high_temperatures[0].text)
+            return result
+
+        else:
+            all_snowfall = soup.find_all("span", attrs={"class": "pjam0"})
+            all_toplevel = soup.find_all("div", attrs={"class": "cuG6ob"})
+            all_forecast_snow = soup.find_all("span", attrs={"class": "TSjJbc"})
+            high_temperatures = soup.find_all("span", attrs={"class": "ZSvVgb"})
+
+            if all_snowfall:
+                result["snowfall72"] = extract_digits(all_snowfall[0].text)
+                result["snowfall48"] = extract_digits(all_snowfall[1].text)
+                result["snowfall24"] = extract_digits(all_snowfall[2].text)
+            if all_toplevel:
+                result["snowDescription"] = all_toplevel[0].text
+                result["runsOpen"] = all_toplevel[1].text
+                result["liftsOpen"] = all_toplevel[2].text
+                result["base"] = extract_digits(all_toplevel[3].text)
+                result["summit"] = extract_digits(all_toplevel[4].text)
+            if all_forecast_snow:
+                result["todaySnowForecast"] = extract_digits(all_forecast_snow[0].text)
+            if high_temperatures:
+                result["todayHighTemperature"] = extract_digits(high_temperatures[0].text)
+            return result
+
+    except IndexError:
+        print("Valid snow report data not found for search url '{}'.".format(search_url))
+        return result
---
+++ snowfall_api.py
@@ -0,0 +1,87 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+from scraper import get_snow_report
+from flask_sqlalchemy import SQLAlchemy
+from apscheduler.schedulers.background import BackgroundScheduler
+from functools import wraps
+
+from resorts import resorts_list
+
+import datetime
+import json
+import jwt
+import atexit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisisthesecretkey'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')  # http://127.0.0.1:5000/route?token=[token here]
+        if not token:
+            return 'Missing token.', 403
+        try:
+            jwt.decode(token, app.config['SECRET_KEY'])
+            return f(*args, **kwargs)
+        except jwt.exceptions.InvalidTokenError:
+            return 'Token is invalid', 403
+
+    return decorated
+
+
+def get_all_snow_reports():
+    for resort, resort_url in resorts_list:
+        result = get_snow_report(resort_url)
+        # Save results to database
+        sr = SnowReport(resort=resort, resort_url=resort_url, json_data=result)
+        db.session.add(sr)
+        db.session.commit()
+        print("Saved snow report for {} to database".format(sr.resort))
+
+
+class SnowReport(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    resort = db.Column(db.String(110), nullable=False)
+    resort_url = db.Column(db.String(120), nullable=False)
+    json_data = db.Column(db.JSON(1200), nullable=False)
+
+    def __repr__(self):
+        return self.resort
+
+
+api = Api(app)
+
+
+class GetSnowReport(Resource):
+    @token_required
+    def get(self, resort):
+        sr_res = SnowReport.query.filter(SnowReport.resort_url == resort).order_by(SnowReport.time.desc()).first()
+        if not sr_res:
+            return 'No snow report for {} found.'.format(resort), 404
+        serialized_report = sr_res.json_data
+        serialized_report.update({'time': str(sr_res.time)})
+        return serialized_report, 200
+
+
+api.add_resource(GetSnowReport, "/snow_report/<string:resort>")
+
+# Schedule snow report scraping
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=get_all_snow_reports, trigger='interval', hours=1)
+scheduler.start()
+# Shutdown the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
+# GENERATE JWT TOKEN
+# print('Token: {}'.format(jwt.encode({'user': 'luke'}, app.config['SECRET_KEY'], algorithm='HS256')))
+
+# Run the server
+if __name__ == '__main__':
+    app.run(debug=True, host='192.168.1.13', port=5000, use_reloader=False)
